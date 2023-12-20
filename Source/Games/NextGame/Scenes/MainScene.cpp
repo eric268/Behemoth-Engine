bfd9e54199f4a89814bc5d457536385a8a6cf94c
eric galway <ericwilliamgalway@gmail.com>
2023-12-19 19:27:31 -0500

    Set up monkey render profiling

--- Source/Games/NextGame/Scenes/MainScene.cpp
+++ Source/Games/NextGame/Scenes/MainScene.cpp
@@ -14,22 +14,22 @@ void MainScene::Init()
 	Behemoth::CameraFactory cameraFactory{};
 	cameraFactory.CreateCamera(registry, true, "Main Camera");
 
- 	Behemoth::DirectionalLightFactory dirLightFactory{};
- 	dirLightFactory.CreateDirectionalLight(registry);
+	Behemoth::DirectionalLightFactory dirLightFactory{};
+	dirLightFactory.CreateDirectionalLight(registry);
 
-	for (int j = 0; j < 100; j++)
+	for (int j = 0; j < 25; j++)
 	{
 		for (int i = -1; i < 2; i++)
 		{
 			ECS::Entity e1 = registry.CreateEntity("Cube 1");
-			registry.AddComponent<Behemoth::MeshComponent>(e1, "cube.obj", "diamond.png");
+			registry.AddComponent<Behemoth::MeshComponent>(e1, "monkey.obj", "diamond.png");
 			registry.AddComponent<Behemoth::TransformComponent>(e1);
 			registry.AddComponent<Behemoth::MeshInitalizeComponent>(e1);
 			registry.AddComponent<Behemoth::RotationComponent>(e1, i + 1, 1.0f);
-			registry.AddComponent<Behemoth::MovementComponent>(e1, Math::Vector3(-3.0f * i, 0.0f, -5.0f));
+			registry.AddComponent<Behemoth::MovementComponent>(e1, Math::Vector3(0.0f * i, 0.0f, -5.0f));
 			registry.AddComponent<Behemoth::ScalingComponent>(e1, Math::Vector3(1.0f, 1.0f, 1.0f));
 
-			// registry.AddComponent<Behemoth::BoundingVolumeComponent>(e1, 1.5f, false);
+			registry.AddComponent<Behemoth::BoundingVolumeComponent>(e1, 1.5f, false);
 
 			Behemoth::PointLightFactory pointLightFactory{};
 			pointLight = pointLightFactory.CreatePointLight(registry, "Point Light 1");
